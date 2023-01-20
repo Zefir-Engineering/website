99906487152de89459035406f3a983178356cfc0
Guillaume Rybak <5887099+guillaumerybak@users.noreply.github.com>
2023-01-20 17:07:30 +0100

    chore: refacto landing.js & add minify & add makefile & add prettier config

---
+++ Makefile
@@ -0,0 +1,20 @@
+OJS := $(wildcard *.js)
+MJS := $(OJS:.js=.min.js)
+DJS := $(addprefix dist/,${MJS})
+
+all: $(DJS)
+.PHONY: all
+
+dist/%.min.js : %.js
+	npx uglify-js $^ --compress --mangle --no-annotations -o $@ --source-map
+
+format:
+	npx prettier -w *.js
+
+install:
+	yarn add -D uglify-js prettier
+
+clean:
+	@rm -rvf node_modules yarn.lock package.json dist/*
+
+re: clean install all
--- landing.js
+++ landing.js
@@ -1,36 +1,59 @@
-$(function () {
-  initAutoComplete($("#autocomplete"), $("#goingnext"));
-  initAutoComplete($("#autocomplete_2"), $("#goingnext-2"));
-  initAutoComplete($("#autocomplete_3"), $("#goingnext-3"));
-  storeDataForProductPitchABTesting();
-});
-
 // used by the marketing team for analytics and AB testing, to track which page,
 // ad or marketing pitch was presented to the seller initially
 function storeDataForProductPitchABTesting() {
   const path = location.pathname.toLowerCase();
 
   const productPitches = [
-    [/\/(lp\/|)max($|\/.*)/, "MAX"],
-    [/\/(lp\/|)cover($|\/.*)/, "COVER"],
-    [/\/(lp\/|)vendre(-|\/).+/, "COVER"],
-    [/\/.*/, "NONE"],
+    [/\/(lp\/|)max($|\/.*)/, 'MAX'],
+    [/\/(lp\/|)cover($|\/.*)/, 'COVER'],
+    [/\/(lp\/|)vendre(-|\/).+/, 'COVER'],
+    [/\/.*/, 'NONE'],
   ];
-  localStorage.setItem("productPitch", productPitches.find((v) => v[0].test(path))[1]);
+  localStorage.setItem('productPitch', productPitches.find((v) => v[0].test(path))[1]);
 }
 
 let showError = true;
 function displayError() {
   if (showError) {
     showError = false;
-    alert("Merci de préciser votre numéro de rue.");
+    alert('Merci de préciser votre numéro de rue.');
     setTimeout(() => (showError = true), 1000);
   }
 }
 
-function useFirstPrediction($input) {
-  const value = $input.val();
-  if (value === "") {
+function createCookieAndRedirect(place) {
+  const componentForm = {
+    street_number: 'short_name',
+    route: 'long_name',
+    locality: 'long_name',
+    administrative_area_level_1: 'short_name',
+    country: 'long_name',
+    postal_code: 'short_name',
+  };
+
+  // for some reason, the first item it ine address_components object should be the street number, if it's not, we manually set the cookie for it
+  if (place.address_components[0].types[0] != 'street_number') {
+    document.cookie = 'street_number=1;path=/';
+  }
+  // Get each component of the address from the place details, and then fill-in the corresponding field on the cookie.
+  for (const addressComponent of place.address_components) {
+    const addressType = addressComponent.types[0];
+
+    const formFieldName = componentForm[addressType];
+    if (!formFieldName) {
+      continue;
+    }
+
+    const value = encodeURIComponent(addressComponent[componentForm[addressType]]);
+    document.cookie = `${addressType}=${value};path=/`;
+  }
+
+  window.location.pathname = '/offre/demande/v2';
+}
+
+function useFirstPrediction(input) {
+  const value = input.val();
+  if (!value) {
     return;
   }
 
@@ -40,110 +63,63 @@ function useFirstPrediction($input) {
   }
 
   const service = new google.maps.places.AutocompleteService();
-  service.getPlacePredictions(
-    { input: value, componentRestrictions: { country: "fr" } },
-    function (predictions) {
-      if (predictions.length > 0) {
-        $input.val(predictions[0].description);
-
-        const geocoder = new google.maps.Geocoder();
-        geocoder
-          .geocode({ placeId: predictions[0].place_id })
-          .then((response) => {
-            createCookieAndRedirect(response.results[0]);
-          });
-      }
+  service.getPlacePredictions({ input: value, componentRestrictions: { country: 'fr' } }, async (predictions) => {
+    if (predictions.length <= 0) {
+      return;
     }
-  );
+
+    input.val(predictions[0].description);
+    const geocoder = new google.maps.Geocoder();
+    const response = await geocoder.geocode({ placeId: predictions[0].place_id });
+    createCookieAndRedirect(response.results[0]);
+  });
 }
 
-function initAutoComplete($input, $button) {
-  // Create the autocomplete object, restricting the search predictions to
-  // adresses and France.
+function initAutoComplete(inputSelector, buttonSelector) {
+  const input = $(inputSelector);
+  const button = $(buttonSelector);
+  // Create the autocomplete object, restricting the search predictions to addresses and France.
   // Address must start with a number to start Autocomplete
-
-  var options = {
-    types: ["address"],
-    componentRestrictions: { country: "fr" },
-  };
-
-  const autocomplete = new google.maps.places.Autocomplete($input[0], options);
+  const autocomplete = new google.maps.places.Autocomplete(input[0], {
+    types: ['address'],
+    componentRestrictions: { country: 'fr' },
+  });
 
   // Avoid paying for data that you don't need by restricting the set of
   // place fields that are returned to just the address components.
-  autocomplete.setFields(["address_component"]);
+  autocomplete.setFields(['address_component']);
 
   // When the user selects an address from the drop-down, save the
   // address fields in local storage.
-  autocomplete.addListener("place_changed", function () {
-    if ($input.val().match(/^\d/)) {
-      const place = autocomplete.getPlace();
-
-      if (typeof place.address_components !== "undefined") {
-        createCookieAndRedirect(place);
-      }
-    } else {
+  autocomplete.addListener('place_changed', () => {
+    if (!/^\d/.test(input.val())) {
       displayError();
+      return;
+    }
+
+    const place = autocomplete.getPlace();
+    if (!!place.address_components) {
+      createCookieAndRedirect(place);
     }
   });
 
-  $button.on("click", function () {
-    useFirstPrediction($input);
+  button.on('click', () => {
+    useFirstPrediction(input);
     return false;
   });
 
-  $input.keypress(function (evt) {
-    if (evt.which == 13) {
-      useFirstPrediction($input);
+  input.keypress((event) => {
+    if (event.which == 13) {
+      // if enter pressed
+      useFirstPrediction(input);
       return false;
     }
   });
 }
 
-var componentForm = {
-  street_number: "short_name",
-  route: "long_name",
-  locality: "long_name",
-  administrative_area_level_1: "short_name",
-  country: "long_name",
-  postal_code: "short_name",
-};
-
-function createCookieAndRedirect(place) {
-  // Get each component of the address from the place details,
-  // and then fill-in the corresponding field on the cookie.
-  for (var i = 0; i < place.address_components.length; i++) {
-    var addressType = place.address_components[i].types[0];
-    if (componentForm[addressType]) {
-      if (i == 0 && place.address_components[0].types[0] != "street_number") {
-        var addressType_1 = "street_number";
-        var val_1 = "1";
-
-        var path = "path=/";
-        var homecookie = addressType_1 + "=" + val_1;
-
-        document.cookie = homecookie + ";" + path;
-
-        var addressType_2 = addressType;
-        var val_2 = encodeURIComponent(
-          place.address_components[i][componentForm[addressType]]
-        );
-
-        var path = "path=/";
-        var homecookie = addressType_2 + "=" + val_2;
-
-        document.cookie = homecookie + ";" + path;
-      } else {
-        var val = encodeURIComponent(
-          place.address_components[i][componentForm[addressType]]
-        );
-        var homecookie = addressType + "=" + val;
-        var path = "path=/";
-
-        document.cookie = homecookie + ";" + path;
-      }
-    }
-  }
-
-  window.location.pathname = "/offre/demande/v2";
-}
+$(function () {
+  initAutoComplete('#autocomplete', '#goingnext');
+  initAutoComplete('#autocomplete_2', '#goingnext-2');
+  initAutoComplete('#autocomplete_3', '#goingnext-3');
+  storeDataForProductPitchABTesting();
+});
