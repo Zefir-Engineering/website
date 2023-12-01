f78b17e76b9561c779366d2194985fb99673eeae
Louis de Courcel <einsenhorn@gmail.com>
2023-12-01 21:51:46 +0100

    Add Redirection to Our Seller Lead Form
    Link with an id attribute starting by "slf_v2" will redirect on the seller lead form url while keeping UTMs

--- seller_form_redirect.js
+++ seller_form_redirect.js
@@ -1,7 +1,22 @@
 $(function () {
-  $("a[id^='seller_form']").on('click', (e) => {
+  $("a[id^='seller_form']").on("click", () => {
     window.location.pathname = "/offre/demande/v2";
-    
+
+    return false;
+  });
+
+  $("a[id^='slf_v2']").on("click", (e) => {
+    const target = $(e.target);
+
+    try {
+      let url = new URL(target.attr("href"));
+
+      url.pathname = "offre/demande/v2";
+      window.location.pathname = url.toString();
+    } catch (e) {
+      window.location.pathname = "/offre/demande/v2";
+    }
+
     return false;
   });
 });
